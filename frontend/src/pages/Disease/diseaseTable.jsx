b6bba8259976bcc6b4611501fff067302eebb81b
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2024-10-03 00:24:47 +0530

    Update diseaseTable.jsx

--- frontend/src/pages/Disease/diseaseTable.jsx
+++ frontend/src/pages/Disease/diseaseTable.jsx
@@ -78,7 +78,7 @@ const ReadDiseases = () => {
             className="w-full h-12 px-4 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-lg"
           />
           <button
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 ml-4"
+            className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 ml-4"
             onClick={() => navigate('/Pest&Disease/diseaseTable/addNew')}
             style={{marginRight:"-300px"}}
           >
