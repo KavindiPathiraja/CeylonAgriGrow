03439a6c3dfc1ef1533ea9f18ad8a9ddd8b8440c
Jayana Winsara <jayana@smartedesigners.com>
2024-10-03 21:55:27 +0530

    backend files

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import diseaseRouter from './routes/diseaseRoute.js';
+import path from 'path';
+import cropRoutes from './routes/selectRoute.js'
+import dotenv from 'dotenv';
+import geminiRouter from './routes/geminiApi.js'; // Import Gemini AI route
+
+// Importing routes
+import Products_Route from './routes/Products_Route.js';
+import Farmers_Route from './routes/Farmers_Route.js';
+import Card_Route from './routes/Card_Route.js';
+
+import Order_Route from './routes/Order_Route.js';
+
+//Import crop routes
+import cropsRouter from "./routes/cropsRoute.js";
+
+
+// Creating an instance of the Express application
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(cors());
+
+// Use the Gemini AI router
+app.use('/generate-response', geminiRouter);
+
+// Using routes for endpoints
+app.use('/products', Products_Route);
+app.use('/farmers', Farmers_Route);
+app.use('/crops', cropsRouter);
+app.use('/card', Card_Route);
+app.use('/order', Order_Route);
+
+
+// Connecting to the MongoDB database
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.use('/diseases', diseaseRouter);
+app.use('/images', express.static(path.join('images')));
+app.use('/api', cropRoutes);
