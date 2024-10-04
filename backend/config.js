a11364bda1a0cf540afc795442a40e3a74696d3c
KavindiPathiraja <155365777+KavindiPathiraja@users.noreply.github.com>
2024-10-04 12:17:47 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,5 @@
 export const PORT = 5556;
 
-<<<<<<< Updated upstream
 export const mongoDBURL = 'mongodb+srv://agri:agri@ceylonagrigrow.iflfn.mongodb.net/agri-productdb?retryWrites=true&w=majority&appName=CeylonAgriGrow'
 
 export const apiKey = 'AIzaSyBMvLY-N-RS0i0NpIHSmXmCMFm410RjVfQ';
-=======
-export const mongoDBURL = 'mongodb+srv://agri:agri@ceylonagrigrow.iflfn.mongodb.net/agri-productdb?retryWrites=true&w=majority&appName=CeylonAgriGrow'
->>>>>>> Stashed changes
