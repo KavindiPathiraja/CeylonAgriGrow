c8287dd79326863767cc5af732085b7b06c07fd9
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2024-10-05 16:14:36 +0530

    button color updated

--- frontend/src/pages/Disease/DiseasePredictionForm.jsx
+++ frontend/src/pages/Disease/DiseasePredictionForm.jsx
@@ -49,7 +49,7 @@ const DiseasePredictionForm = () => {
       {/* Navigation button in the top-right corner */}
       <button
         onClick={() => navigate('/Pest&Disease/selectCrop')}
-        className="absolute top-4 right-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        className="absolute top-4 right-4 bg-secondary border border-black-200 text-white px-4 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
       >
         Find About Disease
       </button>
@@ -81,7 +81,7 @@ const DiseasePredictionForm = () => {
 
           <button
             type="submit"
-            className="w-full px-4 py-2 rounded-md shadow-sm bg-indigo-600 hover:bg-indigo-700 text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full px-4 py-2 rounded-md shadow-sm bg-primary hover:bg-secondary text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring"
           >
             Predict Disease
           </button>
